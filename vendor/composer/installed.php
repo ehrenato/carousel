6defca31c36d75e5f59fd348e019922c98e5b006
RENATO FREIRE CORREIA <ehrenato@gmail.com>
2021-10-22 17:10:27 -0300

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '43b141de22675e56d4cc17412f171acaf6907145',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '43b141de22675e56d4cc17412f171acaf6907145',
+            'dev_requirement' => false,
+        ),
+        'harvesthq/chosen' => array(
+            'pretty_version' => 'v1.8.7',
+            'version' => '1.8.7.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../harvesthq/chosen',
+            'aliases' => array(),
+            'reference' => 'ad86732b668627c131e61ee8f0e6e9ed52e4db8d',
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.5.1',
+            'version' => '6.5.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'reference' => 'dd803df5ad7492e1b40637f7ebd258fee5ca7355',
+            'dev_requirement' => false,
+        ),
+    ),
+);
